d8068c39bc8a8a7a6276f396a60f2ef548f84da8
AlexeiEvenepoel <alexeievn.dev@gmail.com>
2025-07-20 18:29:55 -0500

    feat: implement user authentication display and logout functionality in header

--- frontend/src/app/shared/ui/layout-panel/header/header.component.ts
+++ frontend/src/app/shared/ui/layout-panel/header/header.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { UserDropdownComponent } from './components/user-dropdown.component';
-import { ToggleThemeComponent } from '../../../toggle-theme/toggle-theme.component';
+import { AuthService } from '../../../../auth/data-access/auth.service';
+import { Router } from '@angular/router';
+import { User } from '../../../../auth/interfaces/auth.interface';
 
 @Component({
   selector: 'app-header-panel',
   standalone: true,
-  imports: [CommonModule, UserDropdownComponent, ToggleThemeComponent],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
 })
-export class HeaderPanelComponent {}
+export class HeaderPanelComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  user: User | null = null;
+
+  constructor() {
+    this.authService.currentUser$.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  logOut() {
+    this.authService.logout();
+    this.router.navigate(['/auth/sign-in']);
+  }
+}
